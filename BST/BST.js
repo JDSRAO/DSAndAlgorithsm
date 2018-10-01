aa4b90e19156b948ca486bdb162a4dc7fe1f5885
jdsrao <kjdssrinivasarao@gmail.com>
2018-10-01 18:48:47 +0530

    BST Contains Method

--- BST/BST.js
+++ BST/BST.js
@@ -19,6 +19,37 @@ BST.prototype.Add = function (value) {
     }
 };
 
+BST.prototype.Contains = function (value) {
+    if(!this.root) {
+        return false;
+    }
+    else {
+       return contains(value, this.root);
+    }
+};
+
+let contains = function (value, node) {
+    if(value === node.value) {
+        return true;
+    }
+    else if(value < node.value) {
+        if(node.left) {
+            return contains(value, node.left);
+        }
+        else {
+            return false;
+        }
+    }
+    else if(value > node.value) {
+        if(node.right) {
+            return contains(value, node.right);
+        }
+        else {
+            return false;
+        }
+    }
+};
+
 let AddNode = function (value, node) {
     if(value <= node.value) {
         if(!node.left) {
--- BST/program.js
+++ BST/program.js
@@ -3,8 +3,15 @@
 const bst = require('./BST');
 
 let myBst = new bst.BST();
+let valueToSearch = 4;
+myBst.Add(10);
+myBst.Add(20);
 myBst.Add(1);
 myBst.Add(2);
-myBst.Add(1);
+myBst.Add(3);
+myBst.Add(4);
 
-console.info(JSON.stringify(myBst));
+console.log(`Does BST contains ${valueToSearch} : ${myBst.Contains(valueToSearch)}`);
+console.log(`Does BST contains ${valueToSearch + 60} : ${myBst.Contains(valueToSearch + 60)}`);
+
+console.log(JSON.stringify(myBst), undefined, '\n');
